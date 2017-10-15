50025a8d6a642f3edfc4e42340861f3041031b1e
Kenneth Uyabeme <kuyabeme@gmail.com>
2017-10-15 16:36:15 -0400

    -number of contacts textView added to media info layout xml -adapter class created -listview is not populated on start up

--- app/src/main/java/com/example/kenny/kenneth_uyabeme_a2/MainActivity.java
+++ app/src/main/java/com/example/kenny/kenneth_uyabeme_a2/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.kenny.kenneth_uyabeme_a2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     //Fields
     ListView socialMedialistview ; // ListView containing the social media account info
     CheckBox displayInfoCheckBox; // checkbox to display/hide the account ids
+    SocialMediaAccountAdapter adapter; // adapter for listView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +20,31 @@ public class MainActivity extends Activity {
         //Getting reference to the social media listview and checkbutton
         socialMedialistview = findViewById(R.id.social_media_list_view);
         displayInfoCheckBox = findViewById(R.id.display_info_check_box);
+        loadSocialMediaAccounts();//loading data into the social media listView
+        //Setting checkBox listener
+        displayInfoCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {/*If checked make the userID and number of contacts textViews visible
+            else make them invisible*/
+                if(displayInfoCheckBox.isChecked()){
+                    findViewById(R.id.user_id).setVisibility(View.VISIBLE);
+                    findViewById(R.id.num_of_contacts_textview).setVisibility(View.VISIBLE);
+                }else{
+                    findViewById(R.id.user_id).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.num_of_contacts_textview).setVisibility(View.INVISIBLE);
+                }
 
+            }
+        });
 
+    }
 
+    private void loadSocialMediaAccounts(){
+        //Getting the data the SocialMediaAccount class
+        ArrayList<SocialMediaAccount> accounts = SocialMediaAccount.getSocialMedia();
+        adapter = new SocialMediaAccountAdapter(this,
+                                                     R.layout.list_media_info,
+                                                    accounts);
+        socialMedialistview.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/kenny/kenneth_uyabeme_a2/SocialMediaAccountAdapter.java
@@ -0,0 +1,53 @@
+package com.example.kenny.kenneth_uyabeme_a2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kenny on 2017-10-15.
+ * This is the adapter that will help display the data from SocialMediaAccount POJO
+ * in Social Media Listview
+ */
+
+public class SocialMediaAccountAdapter extends ArrayAdapter {
+    //Fields
+    private Context context;//for getting the LayoutInflator
+    private int resource;// this will be use to get the layout xml for the each social media account
+
+    public SocialMediaAccountAdapter(Context context, int resource, ArrayList<SocialMediaAccount> accounts ) {
+        super(context, resource, accounts);// calling supeer class constructor
+        this.context = context;//setting the context
+        this.resource = resource; //setting the resources
+
+    }
+    @Override
+    //This creates a view for each social media account using the list_media_info layout
+    public View getView(int position, View convertView, ViewGroup parent){
+        if (convertView == null){ // inflating the view if it isn't inflated
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            convertView = inflater.inflate(resource, parent,false);
+        }
+        //Getting the current social media account
+        SocialMediaAccount account = (SocialMediaAccount) getItem(position);
+        //Making a textView reference for the site name, userID and number of contacts textView of that socialMedia account
+        TextView nameTextView = convertView.findViewById(R.id.social_media_site_textview);
+        TextView userIdTextView = convertView.findViewById(R.id.user_id);
+        TextView numberOfContactsTextView = convertView.findViewById(R.id.num_of_contacts_textview);
+        //The populating the TextView with info from the account.
+        nameTextView.setText(account.getName());
+        userIdTextView.setText(account.getUserId());
+        numberOfContactsTextView.setText(Integer.toString(account.getNumberOfContacts()));
+
+
+        //Returning the modified view
+        return convertView;
+    }
+}
