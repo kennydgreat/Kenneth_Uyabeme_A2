cd9b752043bb6fefb7e63b6fa1162069597615ef
Kenneth Uyabeme <kuyabeme@gmail.com>
2017-10-15 11:59:11 -0400

    -main activity UI created -media info layout xml created -Attached UI to main activity -SocialMediaAccount POJO created

--- app/src/main/java/com/example/kenny/kenneth_uyabeme_a2/MainActivity.java
+++ app/src/main/java/com/example/kenny/kenneth_uyabeme_a2/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.kenny.kenneth_uyabeme_a2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+    //Fields
+    ListView socialMedialistview ; // ListView containing the social media account info
+    CheckBox displayInfoCheckBox; // checkbox to display/hide the account ids
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Getting reference to the social media listview and checkbutton
+        socialMedialistview = findViewById(R.id.social_media_list_view);
+        displayInfoCheckBox = findViewById(R.id.display_info_check_box);
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/kenny/kenneth_uyabeme_a2/SocialMediaAccount.java
@@ -0,0 +1,68 @@
+package com.example.kenny.kenneth_uyabeme_a2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kenny on 2017-10-15.
+ * This class is the model which holds the data for the Social Media Accounts apps
+ *
+ */
+
+public class SocialMediaAccount {
+    //Class fields
+    private String name;// Name of the social media site
+    private String userId; // the site's user ID
+    private int numberOfContacts; // the account's number of contacts
+
+    //Constructor
+    public SocialMediaAccount(String name, String userId, int numberOfContacts){
+        //setting fields
+        this.name = name;
+        this.userId = userId;
+        this.numberOfContacts = numberOfContacts;
+    }
+
+    //Name setter and getter methods
+    //---------
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //-------------
+
+    //User ID getter and setters
+    //----------------
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    //Number of Contacts getter and setter
+    //-----------------
+    public int getNumberOfContacts() {
+        return numberOfContacts;
+    }
+
+    public void setNumberOfContacts(int numberOfContacts) {
+        this.numberOfContacts = numberOfContacts;
+    }
+    //-----------------
+
+
+    //This creates the social media account data and returns the data(arraylist)
+    public static ArrayList<SocialMediaAccount> getSocialMedia(){
+        ArrayList<SocialMediaAccount> accounts = new ArrayList<>();
+        accounts.add(new SocialMediaAccount("Facebook", "Vein_doe", 300));
+        accounts.add(new SocialMediaAccount("Instagram", "Vein_DDD", 25));
+        accounts.add(new SocialMediaAccount("Tumblr", "Vein902", 90));
+
+        return accounts;
+    }
+}
